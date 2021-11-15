21aba84492ce937bf622b161d3140b6568e9b9f4
Nitish Kumar <91321018+1999NITISH@users.noreply.github.com>
2021-11-15 16:58:01 +0530

    Add files via upload

---
+++ icdjava.java
@@ -0,0 +1,19 @@
+package assignmentjava;
+
+public class icdjava {
+
+	public icdjava() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int x = 12;
+		    double y = 11.12;
+		    char n = 'n';
+		    System.out.println("int : "+x+"\nfloat : "+y+"\nchar : "+n);
+		
+
+	}
+
+}
