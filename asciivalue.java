de89f5634bd1e31e7f932149c3d2c237b880672a
Nitish Kumar <91321018+1999NITISH@users.noreply.github.com>
2021-11-15 16:57:38 +0530

    Add files via upload

---
+++ asciivalue.java
@@ -0,0 +1,15 @@
+package assignmentjava;
+
+public class asciivalue {
+
+	public asciivalue() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 3+'n';
+		System.out.println((char)x);
+	}
+
+}
