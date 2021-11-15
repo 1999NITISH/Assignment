24428060be9cccaa52b855f8803eca3b86d8926e
Nitish Kumar <91321018+1999NITISH@users.noreply.github.com>
2021-11-15 16:57:49 +0530

    Add files via upload

---
+++ decimal.java
@@ -0,0 +1,16 @@
+package assignmentjava;
+
+public class decimal {
+
+	public decimal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.println(8.2*6);
+
+	}
+
+}
