0d4599755b0f014d0b599cba1a31f27de77d3cf3
Nitish Kumar <91321018+1999NITISH@users.noreply.github.com>
2021-11-15 16:57:27 +0530

    Add files via upload

---
+++ asciichar.java
@@ -0,0 +1,18 @@
+package assignmentjava;
+
+public class asciichar {
+
+	public asciichar() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char n = 'p';
+		int ascii = n;
+		System.out.println("The ASCII value of " + n + " is :" + ascii);
+		
+
+	}
+
+}
